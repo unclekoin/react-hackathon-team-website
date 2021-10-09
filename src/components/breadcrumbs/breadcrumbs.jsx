a90c1a46e864f0e02d44e85cc7d7eeb77c99d2e9
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-10-09 14:02:46 +0300

    added breadcrumbs

--- src/components/breadcrumbs/breadcrumbs.jsx
+++ src/components/breadcrumbs/breadcrumbs.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Breadcrumbs = () => {
-  return (
-    <div>
-      <h1>Breadcrumbs</h1>
-    </div>
-  );
-}
- 
+    const location = useLocation();
+    const breadCrumbsItems = [
+        { to: "/", label: "Home" },
+        { to: "/favourites", label: "Favourites" },
+        { to: "/contact", label: "Contact" },
+        { to: "/about", label: "About" },
+        { to: "/:memberId", label: "Member" }
+    ];
+    return (
+        <div>
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    {breadCrumbsItems
+                        .filter((item) => item.to === location.pathname)
+                        .map(({ to, label }) => (
+                            <Link
+                                key={to}
+                                to={to}
+                                className="breadcrumb-item active"
+                                aria-current="page"
+                            >
+                                {label}
+                            </Link>
+                        ))}
+                </ol>
+            </nav>
+        </div>
+    );
+};
+
 export default Breadcrumbs;
