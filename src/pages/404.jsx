1632144070aa59e451c2b57012d874c508f60c4c
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-10-10 20:35:00 +0700

    fix styles page 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,33 +1,28 @@
 import React from 'react'
-import notFoundPict from '../assets/images/404_notfound.png'
 import { useHistory } from 'react-router'
-import Button from '../components/button/btn'
+import notFoundPict from '../assets/images/404_notfound.png'
+import MenuItem from '../components/navbar/MenuItem'
 
 const PageNotFound = () => {
-	const history = useHistory()
-
-	const goHome = () => {
-		history.replace('/')
-	}
-	return (
-		<>
-			<div className='page-wrapper'>
-				<img
-					style={{
-						display: 'block',
-						width: '60%',
-						marginLeft: 'auto',
-						marginRight: 'auto',
-					}}
-					src={notFoundPict}
-					alt='Page not found'
-				/>
-			</div>
-			<Button onClick={goHome} cls='btn btn-outline-primary btn-lg'>
-				Back to Home
-			</Button>
-		</>
-	)
+  return (
+    <>
+      <div className='page-wrapper-404'>
+        <img
+          style={{
+            display: 'block',
+            width: '60%',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+          }}
+          src={notFoundPict}
+          alt='Page not found'
+        />
+      <div className='block-to-home'>
+        <MenuItem to=''>go to home page :)</MenuItem>
+      </div>
+      </div>
+    </>
+  )
 }
 
 export default PageNotFound
