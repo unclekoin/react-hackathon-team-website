2b2680990c3136b342ce23f85ac42e24a236aead
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-10-10 13:47:06 +0700

    fix css styles index.css slider.css

--- src/pages/favourites.jsx
+++ src/pages/favourites.jsx
@@ -9,18 +9,18 @@ const Favourites = ({ onFavorite }) => {
   const favourites = members.filter((member) => storage[member._id]);
 
   return (
-    <main>
-      <section className="pt-5 pb-3 text-center container">
-        <div className="col-lg-6 col-md-8 mx-auto">
-          <h1 className="fw-light">
+    <main className='page-wrapper'>
+      <section className='pt-5 pb-3 text-center container'>
+        <div className='col-lg-6 col-md-8 mx-auto'>
+          <h1 className='fw-light'>
             {favourites.length ? 'Favourites' : "You haven't chosen anyone yet"}
           </h1>
         </div>
       </section>
       {!!favourites.length && (
-        <div className="album py-5 bg-light p-3">
-          <div className="container">
-            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div className='album py-5 bg-light p-3'>
+          <div className='container'>
+            <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
               {favourites.map((favorite) => (
                 <Card
                   key={favorite._id}
@@ -33,7 +33,7 @@ const Favourites = ({ onFavorite }) => {
         </div>
       )}
     </main>
-  );
+  )
 };
 
 Favourites.propTypes = {
