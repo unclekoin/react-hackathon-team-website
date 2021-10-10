7291c402935f5a675169bebbb16f15c484f5e731
Pavel Koryakin <unclekoin@gmail.com>
2021-10-10 08:41:32 +0300

    feat: add useParams

--- src/pages/member.jsx
+++ src/pages/member.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const Member = () => {
+  const params = useParams();
+  const { memberId } = params;
+  console.log(memberId)
+  
   return (
     <div className='container member-wrapper'>
       <h1>Member</h1>
