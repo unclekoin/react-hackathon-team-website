89d7da6a83977d42206b4ad7a6ea42f717622b01
DmitryLavrov <d3236562@mail.ru>
2021-10-10 12:30:28 +0300

    Breadcrumbs. Last element made unlinked

--- src/components/breadcrumbs/breadcrumbs.jsx
+++ src/components/breadcrumbs/breadcrumbs.jsx
@@ -1,60 +1,64 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
-import members from "../../db/api.members";
+import React from 'react'
+import {Link, useLocation} from 'react-router-dom'
+import members from '../../db/api.members'
 
 const Breadcrumbs = () => {
-	const location = useLocation();
-	const breadCrumbsItems = [
-		{ to: "/", label: "Home" },
-		{ to: "/favourites", label: "Favourites" },
-		{ to: "/contacts", label: "Contacts" },
-		{ to: "/about", label: "About" },
-		{ to: "/:memberId", label: "Member" }
-	];
+  const location = useLocation()
 
-	const membersCrumbs = members.map((member) => {
-		return { to: `/${member._id}`, label: `${member.firstName}` };
-	});
+  const breadCrumbsItems = [
+    {to: '/', label: 'Home'},
+    {to: '/favourites', label: 'Favourites'},
+    {to: '/contacts', label: 'Contacts'},
+    {to: '/about', label: 'About'},
+    {to: '/:memberId', label: 'Member'}
+  ]
 
-	const allBreadCrumbs = [...breadCrumbsItems, ...membersCrumbs];
+  const membersCrumbs = members.map((member) => {
+    return {to: `/${member._id}`, label: `${member.firstName}`}
+  })
 
-	const breadCrumbsData = (location) => {
-		if (location !== "/" && location !== "/404") {
-			return location.split("/").map((crumb) => "/" + crumb);
-		}
-		return [];
-	};
-	const locationArray = breadCrumbsData(location.pathname);
-	const resultData = [];
-	const totalLocation = (arrayPathString, dataPath) => {
-		dataPath.forEach((path) => {
-			arrayPathString.forEach((item) => {
-				if (item === path.to) {
-					resultData.push(path);
-				}
-			});
-		});
-	};
-	totalLocation(locationArray, allBreadCrumbs);
+  const allBreadCrumbs = [...breadCrumbsItems, ...membersCrumbs]
 
-	return (
-    <div className='container pt-3 pb-0 text-center breadcrumb-style'>
-      <nav aria-label='breadcrumb'>
-        <ol className='breadcrumb'>
-          {resultData.map(({ to, label }) => (
-            <Link
-              key={to}
-              to={to}
-              className='breadcrumb-item'
-              aria-current='page'
-            >
-              {label}
-            </Link>
+  const breadCrumbsData = (location) => {
+    if (location !== '/' && location !== '/404') {
+      return location.split('/').map((crumb) => '/' + crumb)
+    }
+    return []
+  }
+
+  const locationArray = breadCrumbsData(location.pathname)
+
+  const totalLocation = (arrayPathString, dataPath) => {
+    const data = []
+    dataPath.forEach((path) => {
+      arrayPathString.forEach((item) => {
+        if (item === path.to) {
+          data.push(path)
+        }
+      })
+    })
+    return data
+  }
+
+  const resultData = totalLocation(locationArray, allBreadCrumbs)
+  const lastDataItem = resultData.pop()
+
+  return (
+    <div className="container pt-3 pb-0 text-center breadcrumb-style">
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          {resultData.map(({to, label}) => (
+            <li key={to} className="breadcrumb-item">
+              <Link to={to}>{label}</Link>
+            </li>
           ))}
+          <li className="breadcrumb-item" aria-current="page">
+            {lastDataItem.label}
+          </li>
         </ol>
       </nav>
     </div>
   )
-};
+}
 
-export default Breadcrumbs;
+export default Breadcrumbs
